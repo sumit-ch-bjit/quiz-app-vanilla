1f47fdc1a99e94b6f0ad54c7e646237b52f514d2
sumit-ch-bjit <sumit.chakraborty@bjitacademy.com>
2023-08-09 16:16:24 +0600

    add timer to the app

--- js/script.js
+++ js/script.js
@@ -1,125 +1,140 @@
-const questions = [
-    {
-        question: "What is the capital of France?",
-        options: ["Paris", "Madrid", "Berlin", "Rome"],
-        answer: "Paris"
-    },
-    {
-        question: "Which planet is known as the Red Planet?",
-        options: ["Mars", "Venus", "Jupiter", "Saturn"],
-        answer: "Mars"
-    },
-    {
-        question: "What is the largest mammal in the world?",
-        options: ["Blue Whale", "African Elephant", "Giraffe", "Hippopotamus"],
-        answer: "Blue Whale"
-    },
-    {
-        question: "Which gas is most abundant in Earth's atmosphere?",
-        options: ["Oxygen", "Nitrogen", "Carbon Dioxide", "Helium"],
-        answer: "Nitrogen"
-    },
-    {
-        question: "Who painted the Mona Lisa?",
-        options: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Michelangelo"],
-        answer: "Leonardo da Vinci"
-    },
-    {
-        question: "What is the largest planet in our solar system?",
-        options: ["Mars", "Saturn", "Jupiter", "Neptune"],
-        answer: "Jupiter"
-    },
-    {
-        question: "What is the national flower of Japan?",
-        options: ["Tulip", "Rose", "Cherry Blossom", "Daisy"],
-        answer: "Cherry Blossom"
-    },
-    {
-        question: "Which famous scientist developed the theory of relativity?",
-        options: ["Isaac Newton", "Albert Einstein", "Stephen Hawking", "Galileo Galilei"],
-        answer: "Albert Einstein"
-    },
-    {
-        question: "In which year did the Titanic sink?",
-        options: ["1912", "1905", "1920", "1931"],
-        answer: "1912"
-    },
-    {
-        question: "What is the capital of Australia?",
-        options: ["Melbourne", "Sydney", "Canberra", "Perth"],
-        answer: "Canberra"
-    },
-    {
-        question: "What is the chemical symbol for the element gold?",
-        options: ["Au", "Ag", "Gd", "Go"],
-        answer: "Au"
-    },
-    {
-        question: "Which planet is known as the 'Morning Star' or 'Evening Star'?",
-        options: ["Mars", "Mercury", "Venus", "Uranus"],
-        answer: "Venus"
-    }
-];
-
+///selects the dom elements to which other elements will be added
 const questionElement = document.querySelector(".question");
 const optionsElement = document.querySelector(".options");
 const submitButton = document.getElementById("submit");
 const resultElement = document.getElementById("result");
 const startOverButton = document.getElementById("start-over");
+const timeDisplay = document.querySelector(".time");
 
+///two variables to keep currentQuestionIndex and maintain score
 let currentQuestionIndex = 0;
 let score = 0;
+let questions = []; // stores the questions coming from api
+let timer;
 
+function startTimer() {
+  let secondsRemaining = 10; // Set the initial time (in seconds)
+
+  timer = setInterval(() => {
+    if (secondsRemaining <= 0) {
+      clearInterval(timer); // Clear the interval when time is up
+      handleTimeUp();
+    } else {
+      // Update the timer display
+      timeDisplay.textContent = secondsRemaining;
+      secondsRemaining--;
+    }
+  }, 1000); // 1000 milliseconds = 1 second
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
+
+//loads the question from a json-server api
+async function loadQuestions() {
+  try {
+    const response = await fetch("http://localhost:3001/questions");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error loading questions:", error);
+    return []; // returns empty array if no questions found
+  }
+}
+
+// calls loadQuestions and if questions found
+//calls displayQuestions
+async function initializeQuiz() {
+  const loadedQuestions = await loadQuestions();
+  if (loadedQuestions.length > 0) {
+    // questions loaded
+    questions = loadedQuestions;
+    displayQuestion(currentQuestionIndex); /// displayQuestion is invoked with currentQuestionIndex
+  } else {
+    console.error("No questions found."); // prints error message to the console
+  }
+}
+
+// adds elements from options in the questions array and appends to optionsElement
 function displayQuestion(questionIndex) {
-    const question = questions[questionIndex];
-    questionElement.textContent = question.question;
+  // startTimer();
+  const question = questions[questionIndex];
+  questionElement.textContent = question.question;
 
-    optionsElement.innerHTML = "";
-    question.options.forEach((option, index) => {
-        const optionElement = document.createElement("label");
-        optionElement.innerHTML = `
+  optionsElement.innerHTML = "";
+  question.options.forEach((option) => {
+    // for every option in the options array
+    const optionElement = document.createElement("label"); // a option element is created
+    ///string literals to add elements to optionElement
+    optionElement.innerHTML = `
       <input type="radio" name="option" value="${option}">
       ${option}
     `;
-        optionsElement.appendChild(optionElement);
-    });
+    optionsElement.appendChild(optionElement); // child elements are added to the optionsElements
+  });
+
+  timeDisplay.textContent = 10; // Reset time to 10 seconds
+  startTimer();
 }
 
+function handleTimeUp() {
+  stopTimer(); // Clear the timer
+  checkAnswer();
+}
+
+/// checks the answers and increases score
+///displays the next question
+//until the current question index is less than question array's length
 function checkAnswer() {
-    const selectedOption = document.querySelector('input[name="option"]:checked');
-    if (!selectedOption) return;
+  stopTimer();
+  //query selector selects the input element that has the name 'option' and the value is checked
+  const selectedOption = document.querySelector('input[name="option"]:checked');
 
-    const answer = selectedOption.value;
-    if (answer === questions[currentQuestionIndex].answer) {
-        score++;
-    }
+  if (!selectedOption) {
+    handleTimeUp();
+    return;
+  }
 
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-        displayQuestion(currentQuestionIndex);
-    } else {
-        showResult();
-    }
+  const answer = selectedOption.value; /// gets the value of selected elements
+  if (answer === questions[currentQuestionIndex].answer) {
+    score++;
+    //if selected answer matches the answer in questions array -- increase the score
+  }
+
+  currentQuestionIndex++; // increase current question index by 1 to load next question
+  if (currentQuestionIndex < questions.length) {
+    // if current question index less than the question length
+    displayQuestion(currentQuestionIndex); // calls displayQuestion for the next question
+  } else {
+    showResult();
+  }
+  // clearInterval(timer);
 }
 
+// shows the total value of score and
+//makes other elements empty and not displayed on the screen
 function showResult() {
-    questionElement.textContent = "";
-    optionsElement.innerHTML = "";
-    submitButton.style.display = "none";
-    resultElement.textContent = `Your score: ${score} out of ${questions.length}`;
-    startOverButton.style.display = "block";
+  questionElement.textContent = "";
+  optionsElement.innerHTML = "";
+  submitButton.style.display = "none";
+  resultElement.textContent = `Your total score: ${score} out of ${questions.length}`;
+  startOverButton.style.display = "block";
 }
 
+//starts the quiz from beginning with zero score
 function startOver() {
-    currentQuestionIndex = 0;
-    score = 0;
-    resultElement.textContent = "";
-    startOverButton.style.display = "none";
-    displayQuestion(currentQuestionIndex);
-    submitButton.style.display = "block";
+  currentQuestionIndex = 0;
+  score = 0;
+  resultElement.textContent = "";
+  startOverButton.style.display = "none";
+  displayQuestion(currentQuestionIndex);
+  submitButton.style.display = "block";
 }
 
+//add two event handlers to submitButton and startOverButton
 submitButton.addEventListener("click", checkAnswer);
 startOverButton.addEventListener("click", startOver);
 
-displayQuestion(currentQuestionIndex);
+//initializes the quiz
+initializeQuiz();
